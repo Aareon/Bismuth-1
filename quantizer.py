36f810b92f3ab32807135f132b01df818fe462ea
Aareon Sullivan <askully13@gmail.com>
2020-12-10 16:11:34 -0600

    Update quantizer.py
    Add generic `quantize` function that accepts an integer for number of decimal points

--- quantizer.py
+++ quantizer.py
@@ -5,6 +5,14 @@ DECIMAL_ZERO_8DP = Decimal('0.00000000')
 DECIMAL_ZERO_10DP = Decimal('0.0000000000')
 
 
+def quantize(value, dp: int=DECIMAL_ZERO_10DP):
+    value = Decimal(value)
+    if dp in (DECIMAL_ZERO_2DP, DECIMAL_ZERO_8DP, DECIMAL_ZERO_10DP,):
+        return value.quantize(dp)
+    elif isinstance(dp, int) and dp <= 10:
+        return Decimal(f"0.{'0' * dp}")
+
+
 def quantize_two(value):
     if not value:
         return DECIMAL_ZERO_2DP
